5fff9571186d874ccc40ed61a9d6ef860b3158e3
thaoeder <ederthao@web.de>
2016-01-27 23:05:43 +0000

    trans

--- Poem/sketch.js
+++ Poem/sketch.js
@@ -5,3 +5,7 @@ function setup() {
 function draw() {
     
 }
+
+function googleTranslateElementInit() {
+		new google.translate.TranslateElement({pageLanguage: 'en', autoDisplay: false, multilanguagePage: true}, 'google_translate_element');
+		}
